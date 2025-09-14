79599215145cc6f54d3d79f798b358fffda7c2b0
Bawerek <bartoszkrongauz51@gmail.com>
2025-09-14 13:39:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+import discord  # dodajemy import discord
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-bot = commands.Bot(command_prefix="!")
+# Tutaj definiujemy intents
+intents = discord.Intents.default()
+intents.message_content = True  # dzięki temu bot może odczytywać treść wiadomości
+
+# Tworzymy bota z intents
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
